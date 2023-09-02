ec92ed5638b962ee64c31a9356a14254db038acb
Thomas Peruch <peruchthomas@gmail.com>
2023-09-02 12:17:25 -0300

    initial commit

---
+++ src/main/java/com/tperuch/edstest/EdsTestApplication.java
@@ -0,0 +1,13 @@
+package com.tperuch.edstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tperuch/edstest/EdsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tperuch.edstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
