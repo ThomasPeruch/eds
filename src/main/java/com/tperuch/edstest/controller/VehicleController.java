036c2abec7867bf865e567fa47510bddc7f0f765
Thomas Peruch <peruchthomas@gmail.com>
2023-09-03 16:35:24 -0300

    add swagger/openApi config and tags

--- src/main/java/com/tperuch/edstest/controller/VehicleController.java
+++ src/main/java/com/tperuch/edstest/controller/VehicleController.java
@@ -3,6 +3,8 @@ package com.tperuch.edstest.controller;
 import com.tperuch.edstest.dto.SearchVehicleCriteria;
 import com.tperuch.edstest.dto.VehicleDto;
 import com.tperuch.edstest.service.VehicleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -13,16 +15,19 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "veiculos")
+@Tag(name = "Veículos", description = "Operações relacionadas a veículos")
 public class VehicleController {
 
     @Autowired
     private VehicleService vehicleService;
 
+    @Operation(summary = "Registra um novo veículo")
     @PostMapping
     public ResponseEntity<VehicleDto> saveVehicle(@RequestBody @Valid VehicleDto vehicleDto){
         return new ResponseEntity(vehicleService.saveVehicle(vehicleDto), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Busca paginada de veículos, por padrão aparecerão 5 veículos por página")
     @GetMapping
     public ResponseEntity<Page<VehicleDto>> getPaginatedVehicles(
             @RequestParam(value = "page", defaultValue = "0") Integer page,
@@ -32,6 +37,7 @@ public class VehicleController {
         return new ResponseEntity(vehicleService.findAll(pageRequest), HttpStatus.OK);
     }
 
+    @Operation(summary = "Busca por veículo especifico, usar filtros para busca")
     @GetMapping(value = "/search")
     public ResponseEntity<Page<VehicleDto>> getPaginatedAndFilteredVehicles(
             @Valid SearchVehicleCriteria searchVehicleCriteria,
@@ -43,16 +49,19 @@ public class VehicleController {
         return new ResponseEntity(vehicleService.findAllSpecificated(pageRequest,searchVehicleCriteria), HttpStatus.OK);
     }
 
+    @Operation(summary = "Busca veículo por Id")
     @GetMapping(value = "/{id}")
     public ResponseEntity<VehicleDto> getVehicleById(@PathVariable Long id){
         return new ResponseEntity(vehicleService.findVehicleById(id), HttpStatus.OK);
     }
 
+    @Operation(summary = "Edita veículo por Id")
     @PutMapping(value = "/{id}")
     public ResponseEntity<VehicleDto> updateVehicleById(@PathVariable Long id, @RequestBody @Valid VehicleDto vehicleDto){
         return new ResponseEntity(vehicleService.updateVehicleById(id,vehicleDto), HttpStatus.OK);
     }
 
+    @Operation(summary = "Deleta veículo por Id")
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<VehicleDto> deleteVehicleById(@PathVariable Long id){
         vehicleService.deleteVehicleById(id);
